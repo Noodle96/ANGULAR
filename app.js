aac5fcc6b4cbed6952bd081de6baea8a1c43954f
Noodle96 <jorgealfredomarino2@gmail.com>
2020-03-27 01:05:53 -0500

    funcione con parametros obligatorios, opcionales y por defecto.

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ var numero = 7;
 if (true) {
     numero = 11;
 }
-console.log(numero); // print 11
+// console.log(numero); // print 11
 var mensaje = "hola";
 if (true) {
     var mensaje_1 = "decir adios";
 }
-console.log(mensaje); //print hola, debido a que let funciona en un contexto dado
+// console.log(mensaje); //print hola, debido a que let funciona en un contexto dado
 var PI = 3.14;
-console.log(PI);
+// console.log(PI);
 var nombre = "Pedro Pablo";
 var numeroTwo = 123;
 var booleano = true;
 var hoy = new Date();
-console.log(hoy);
+// console.log(hoy);
 var cualquiera;
 cualquiera = nombre;
 console.log(cualquiera);
@@ -31,5 +31,20 @@ console.log(cualquiera);
 var nombree = "Jorge";
 var apellido = "Tito";
 var fechaNacimiento = 2000;
-var texto = "Welcome, " + nombree + " " + apellido + "\n" + fechaNacimiento;
-console.log(texto);
+// let texto = `Welcome, ${nombree} ${apellido}
+// ${fechaNacimiento}`;
+// console.log(texto);]
+//funciones y Parametros obigatorio, por defecto y opcionales
+function mandarSaludo(origen, llegada, valor) {
+    if (llegada === void 0) { llegada = "Escuela CS"; }
+    if (valor) {
+        console.log(origen + " manda saludos a " + llegada + " con el valor de " + valor);
+    }
+    else {
+        console.log(origen + " manda saludos a " + llegada + ".");
+    }
+}
+mandarSaludo("Pedro"); // Pedro manda saludos a Escuela CS
+mandarSaludo("Pedro", "Director de CS"); //Pedro manda saludos a Director de CS.
+mandarSaludo("Pedro", "Director de CS", "Responsabilidad"); //Pedro manda saludos a Director de CS con el valor de Responsabilidad
+//end
--- app.ts
+++ app.ts
@@ -6,17 +6,17 @@
  if (true) {
    numero = 11;
  }
- console.log(numero); // print 11
+ // console.log(numero); // print 11
 
 
 let mensaje  = "hola"
 if (true) {
   let  mensaje = "decir adios";
 }
-console.log(mensaje); //print hola, debido a que let funciona en un contexto dado
+// console.log(mensaje); //print hola, debido a que let funciona en un contexto dado
 
 const PI = 3.14;
-console.log(PI);
+// console.log(PI);
 
 
 let nombre:string  = "Pedro Pablo";
@@ -24,7 +24,7 @@ let numeroTwo:number = 123;
 let booleano:boolean = true;
 let hoy = new Date();
 
-console.log(hoy);
+// console.log(hoy);
 let cualquiera:any;
 cualquiera = nombre; console.log(cualquiera);
 cualquiera = numeroTwo; console.log(cualquiera);
@@ -35,6 +35,40 @@ let nombree:string = "Jorge";
 let apellido:string = "Tito";
 let fechaNacimiento:number = 2000;
 
-let texto = `Welcome, ${nombree} ${apellido}
-${fechaNacimiento}`;
-console.log(texto);
+// let texto = `Welcome, ${nombree} ${apellido}
+// ${fechaNacimiento}`;
+// console.log(texto);]
+
+
+
+
+//funciones y Parametros obigatorio, por defecto y opcionales
+
+function mandarSaludo(origen:string,llegada:string="Escuela CS", valor?:string){
+    if(valor){
+        console.log(`${origen} manda saludos a ${llegada} con el valor de ${valor}`)
+    }else{
+        console.log(`${origen} manda saludos a ${llegada}.`)
+    }
+}
+mandarSaludo("Pedro");// Pedro manda saludos a Escuela CS
+
+mandarSaludo("Pedro","Director de CS");//Pedro manda saludos a Director de CS.
+
+mandarSaludo("Pedro","Director de CS","Responsabilidad");//Pedro manda saludos a Director de CS con el valor de Responsabilidad
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//end
