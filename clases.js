9c3da06c96cecf8693c9e49d2da243d348f2b749
Noodle96 <jorgealfredomarino2@gmail.com>
2020-03-31 22:50:38 -0500

    change class

--- clases.js
+++ clases.js
@@ -2,8 +2,6 @@
 //CLASES EN TYPESCRIPT
 var Carta = /** @class */ (function () {
     function Carta(name, tipo, puntosVida) {
-        if (puntosVida === void 0) { puntosVida = 0; }
-        this.puntosVida = 800;
         this.createInitial = false;
         this.name = name;
         this.tipo = tipo;
@@ -11,5 +9,5 @@ var Carta = /** @class */ (function () {
     }
     return Carta;
 }());
-var princess = new Carta("princesa", "Legendaria");
+var princess = new Carta("princesa", "Legendaria", 3400);
 console.log(princess);
--- clases.ts
+++ clases.ts
@@ -4,15 +4,15 @@
 class Carta {
     name:string;
     tipo:string;
-    puntosVida:number=800;
+    puntosVida:number;
     createInitial:boolean = false;
 
-    constructor(name:string,tipo:string,puntosVida:number=0){
+    constructor(name:string,tipo:string,puntosVida:number){
         this.name=name;
         this.tipo = tipo;
         this.puntosVida=puntosVida;
     }
 }
 
-let princess:Carta = new Carta("princesa","Legendaria");
+let princess:Carta = new Carta("princesa","Legendaria",3400);
 console.log(princess);
